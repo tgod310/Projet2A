cadb156296cf913592caaf1877e2ed0603737907
Yann <yann.darchen@gmail.com>
2020-03-02 14:29:04 +0100

    cos en sin

--- main/fonction/projection.m
+++ main/fonction/projection.m
@@ -1,5 +1,5 @@
 function [model,radar] = projection(model,radar)
 %projction : réalise la projection de U et V sur le rayon du radar
-model.Vr=model.interp_U.*cos(radar.angle)+model.interp_V.*cos(pi/2-radar.angle);
+model.Vr=model.interp_U.*cos(radar.angle)+model.interp_V.*sin(radar.angle);
 end
 
