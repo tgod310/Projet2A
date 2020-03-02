aec9f67c5c7d4efaced1cea8acb7026179a08017
Théo <theogodard@mailoo.org>
2020-03-02 13:03:07 +0100

    comparaison plage spatiale

--- main/main.m
+++ main/main.m
@@ -32,7 +32,7 @@ model.time_max=max(model.time);
 model.time_step=(model.time_max-model.time_min)/length(model.time);
 
 if radar.time_min > model.time_max || radar.time_max < model.time_min
-    error("Les donnees a comparer n'ont pas de plages de donnees communes");
+    error("Les donnees a comparer n'ont pas de temps communes");
 elseif radar.time_max > model.time_min
     time_0=model.time_min;
     time_end=min(model.time_max,radar.time_max);
@@ -40,6 +40,45 @@ elseif radar.time_min < model.time_max
     time_0=radar.time_min;
     time_end=min(model.time_max,radar.time_max);
 end
+
+% Recuperation des plages de donnees commune dans l'espace
+radar.lat_min=min(min(radar.lat));
+radar.lat_max=max(max(radar.lat));
+radar.lon_min=min(min(radar.lon));
+radar.lon_max=max(max(radar.lon));
+model.lat_min=min(min(model.lat));
+model.lat_max=max(max(model.lat));
+model.lon_min=min(min(model.lon));
+model.lon_max=max(max(model.lon));
+
+if radar.lat_min > model.lat_max || radar.lat_max < model.lat_min
+    error("Les donnees a comparer n'ont pas de latitudes communes");
+elseif radar.lat_max > model.lat_min
+    lat_0=model.lat_min;
+    lat_end=min(model.lat_max,radar.lat_max);
+elseif radar.lat_min < model.lat_max
+    lat_0=radar.lat_min;
+    lat_end=min(model.lat_max,radar.lat_max);
+end
+
+if radar.lon_min > model.lon_max || radar.lon_max < model.lon_min
+    error("Les donnees a comparer n'ont pas de longitudes communes");
+elseif radar.lon_max > model.lon_min
+    lon_0=model.lon_min;
+    lon_end=min(model.lon_max,radar.lon_max);
+elseif radar.lon_min < model.lon_max
+    lon_0=radar.lon_min;
+    lon_end=min(model.lon_max,radar.lon_max);
+end
+
+%% A modifier, ne fonctionne pas, but : garder que les lat et lon qui sont dans la zone commune de l'espace
+i_lat=find(radar.lat<lat_0 & radar.lat>lat_end);
+i_lon=find(radar.lon>=lon_0 & radar.lon<=lon_end);
+
+radar.lat(i_lat)=NaN;
+radar.lon(i_lon)=NaN;
+radar.Vr(i_lat)=NaN;
+radar.Vr(i_lon)=NaN;
 
 [model,radar]=interpolation(model,radar);
 [model,radar]=projection(model,radar);
@@ -49,8 +88,17 @@ contourf(radar.lon,radar.lat,radar.Vr(:,:,1));
 colorbar
 c=caxis;
 title('Radar')
+
 figure()
 contourf(radar.lon,radar.lat,model.Vr(:,:,1));
 colorbar
-title('Modele')
+title('Projection du modele')
 caxis(c);
+
+figure()
+contourf(model.lon,model.lat,model.norm(:,:,1));
+colorbar
+title('Norme du modele')
+caxis(c);
+
+
