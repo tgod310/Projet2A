08a850aa3919261f4142823954fe725a14d8da30
Théo <theogodard@mailoo.org>
2020-03-04 11:17:36 +0100

    création struct 'shared'

--- main/fonction/projection.m
+++ main/fonction/projection.m
@@ -1,5 +1,5 @@
 function [model,radar] = projection(model,radar)
-%projction : réalise la projection de U et V sur le rayon du radar
-model.Vr=model.interp_U.*cos(radar.angle)+model.interp_V.*sin(radar.angle);
+%projection : realise la projection de U et V sur le rayon du radar
+model.Vr=model.interp_U.*cosd(radar.angle)+model.interp_V.*sind(radar.angle);
 end
 
