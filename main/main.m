a8b7acb8ad1c9db51ceeef23f4d2c3569ca07cae
Théo <theogodard@mailoo.org>
2020-04-08 11:36:05 +0200

    data1 data2

--- main/main.m
+++ main/main.m
@@ -14,8 +14,8 @@ shared.time_origin='2010-01-01 00:00:00';
 
 %% Recuperation des donnees
 %%%Comparaison radar model%%%
-radar=read_RADAR('20190300001_20191002301_PEY_L1.nc');
-model=read_MODEL('1_NIDOR_20190202_20190215_grid_U.nc','1_NIDOR_20190202_20190215_grid_V.nc');
+data1=read_RADAR('20190300001_20191002301_PEY_L1.nc');
+datd2=read_MODEL('1_NIDOR_20190202_20190215_grid_U.nc','1_NIDOR_20190202_20190215_grid_V.nc');
 
 %%%Comparaison drifter model%%%
 %drifter=read_DRIFTER('033.xlsx');
@@ -23,17 +23,17 @@ model=read_MODEL('1_NIDOR_20190202_20190215_grid_U.nc','1_NIDOR_20190202_2019021
 
 %% Uniformisation du temps
 shared.time_origin_julien=datenum(shared.time_origin); % origine des temps en calendrier julien
-radar.time=radar.time+radar.time_origin-shared.time_origin_julien; % temps radar sur origine des temps
-model.time=model.time/(60*60*24)+model.time_origin-shared.time_origin_julien; % temps model sur origine des temps
+data1.time=data1.time+data1.time_origin-shared.time_origin_julien; % temps radar sur origine des temps
+datd2.time=datd2.time/(60*60*24)+datd2.time_origin-shared.time_origin_julien; % temps model sur origine des temps
 %drifter.time=drifter.time-shared.time_origin_julien; %temps drifter sur origine des temps
 
-[model,radar,shared]=shared_time(model,radar,shared); % recupération des plages temps communes
+[datd2,data1,shared]=shared_time(datd2,data1,shared); % recupération des plages temps communes
 
 %% Uniformisation de l'espace
-[model,radar,shared]=shared_space(model,radar,shared); % recuperation des plages espace communes
+[datd2,data1,shared]=shared_space(datd2,data1,shared); % recuperation des plages espace communes
 
-[model,radar]=interpolation(model,radar,shared); % interpolation model sur espace et moyenne radar sur temps
-[model,radar]=projection(model,radar); % projection model sur radiale du radar
+[datd2,data1]=interpolation(datd2,data1,shared); % interpolation model sur espace et moyenne radar sur temps
+[datd2,data1]=projection(datd2,data1); % projection model sur radiale du radar
 
 
 %% Affichage
@@ -41,7 +41,7 @@ jour=1; % choix du jour a afficher
 
 % Moyenne radar sur le jour
 figure()
-contourf(shared.lon,shared.lat,radar.interp_Vr(:,:,jour));
+contourf(shared.lon,shared.lat,data1.interp_Vr(:,:,jour));
 colorbar
 c=caxis;
 title('Moyenne radar par jour')
@@ -49,8 +49,8 @@ title('Moyenne radar par jour')
 % Norme du Modele
 figure()
 hold on
-contourf(model.lon,model.lat,model.norm(:,:,jour));
-quiver(model.lon,model.lat,model.U(:,:,jour),model.V(:,:,jour),'LineWidth',0.75,'AutoScaleFactor',1.5,'Color','r')
+contourf(datd2.lon,datd2.lat,datd2.norm(:,:,jour));
+quiver(datd2.lon,datd2.lat,datd2.U(:,:,jour),datd2.V(:,:,jour),'LineWidth',0.75,'AutoScaleFactor',1.5,'Color','r')
 hold off
 colorbar
 caxis(c);
@@ -59,8 +59,8 @@ title('Norme du modele')
 % Interpolation du modele
 figure()
 hold on
-contourf(shared.lon,shared.lat,sqrt(model.interp_U(:,:,jour).^2+model.interp_V(:,:,jour).^2))
-quiver(shared.lon,shared.lat,model.interp_U(:,:,jour),model.interp_V(:,:,jour),'LineWidth',0.75,'AutoScaleFactor',1.5,'Color','r')
+contourf(shared.lon,shared.lat,sqrt(datd2.interp_U(:,:,jour).^2+datd2.interp_V(:,:,jour).^2))
+quiver(shared.lon,shared.lat,datd2.interp_U(:,:,jour),datd2.interp_V(:,:,jour),'LineWidth',0.75,'AutoScaleFactor',1.5,'Color','r')
 hold off
 colorbar
 caxis(c)
@@ -69,15 +69,15 @@ title('Interpolation du modele')
 % Projection du modele
 figure()
 hold on
-contourf(shared.lon,shared.lat,model.Vr(:,:,jour))
+contourf(shared.lon,shared.lat,datd2.Vr(:,:,jour))
 hold off
 colorbar
 caxis(c)
 title('Projection du modele')
 
 %% Comparaison
-shared.difference=(abs(model.Vr)-abs(radar.interp_Vr)).^2/max(abs(model.Vr(:,:,:)),[],'all','omitnan')^2; % calcul de la difference entre radar et model
-shared.difference2=abs(abs(model.Vr)-abs(radar.interp_Vr));
+shared.difference=(abs(datd2.Vr)-abs(data1.interp_Vr)).^2/max(abs(datd2.Vr(:,:,:)),[],'all','omitnan')^2; % calcul de la difference entre radar et model
+shared.difference2=abs(abs(datd2.Vr)-abs(data1.interp_Vr));
 
 figure()
 contourf(shared.lon,shared.lat,shared.difference(:,:,jour))
