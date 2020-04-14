772e5d4db9d42b8eb1604f7e7fe715f9a7f4b749
Yann <yann.darchen@gmail.com>
2020-04-14 11:08:13 +0200

    update

--- main/fonction/crossed_data.m
+++ main/fonction/crossed_data.m
@@ -1,7 +1,9 @@
 function [data_0,data_end] = crossed_data(data1_min,data1_max,data2_min,data2_max)
 %crée une plage commune de données
 if data1_min > data2_max || data1_max < data2_min
-    error("Les donnees a comparer n'ont pas de plages communes");
+   disp("Les donnees a comparer n'ont pas de plages communes");
+    data_0=0;
+    data_end=1;
 elseif data1_max > data2_min && data1_max < data2_max
     data_0=max(data1_min,data2_min);
     data_end=data1_max;
